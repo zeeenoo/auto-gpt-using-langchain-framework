3b80d8298a108b2e3a7c9cb26cbc6a5a4138bafa
zeeenoo <a_marhoum@estin.dz>
2023-06-22 20:46:23 +0100

    added the history memory

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import streamlit as st
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain,SimpleSequentialChain,SequentialChain
+from langchain.memory import ConversationBufferMemory
+from langchain.utilities import WikipediaAPIWrapper
+
 os.environ['OPENAI_API_KEY'] = apikey
 
 
@@ -22,10 +25,15 @@ script_template = PromptTemplate(
     template= 'write me a youtube video script base on this title :  {title}'
 )
 
+# Memory :for storing the history
+memory = ConversationBufferMemory(input_key='topic' , memory_key='chat_history')
+
+
+
 #LLM 
 llm = OpenAI(temperature=0.9)
-title_chain = LLMChain (llm=llm , prompt=title_template, verbose=True, output_key='title')
-script_chain = LLMChain (llm=llm , prompt=script_template, verbose=True, output_key='script')
+title_chain = LLMChain (llm=llm , prompt=title_template, verbose=True, output_key='title',memory=memory)
+script_chain = LLMChain (llm=llm , prompt=script_template, verbose=True, output_key='script', memory=memory)
 
 #here we want to concatinate the chains 
 
@@ -37,6 +45,12 @@ sequential_chain = SequentialChain(chains=[title_chain,script_chain], input_vari
 if prompt : 
     # response = llm(prompt)
     response = sequential_chain.run( {'topic': prompt} )
-    st.write(response)
+    st.write(response['title'])
+    st.write(response['script'])
+
+
+    with st.expander('Message History'):
+        st.info(memory.buffer) # like accordion expander
+
 
 
